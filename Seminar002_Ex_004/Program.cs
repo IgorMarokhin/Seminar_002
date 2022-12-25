1f2c21804a76dee2b4d8b5f253f0bd4b7a388a9d
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-25 11:17:37 +0300

    Добавлен проект: решение задачи номер 14

---
+++ Seminar002_Ex_004/Program.cs
@@ -0,0 +1,10 @@
+﻿int number;
+while (true)
+{
+    Console.Write("Введите целое число: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+if ((number % 7 == 0) && (number % 23 == 0)) Console.WriteLine($"{number} -> да");
+else Console.WriteLine($"{number} -> нет");
