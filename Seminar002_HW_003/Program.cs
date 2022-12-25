17dbf2680f2cf471330e1b1be6c797d32f19f1af
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-25 13:04:13 +0300

    Добавлено решение домашнего задания: Задача 15

---
+++ Seminar002_HW_003/Program.cs
@@ -0,0 +1,15 @@
+﻿int number;
+while (true)
+{
+    Console.Write("Введите целое число от 1 до 7, обозначающее день недели: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        if (number >= 1 && number <= 7)
+            break;
+    Console.WriteLine("Ошибка ввода!");
+}
+if (number == 6)
+    Console.WriteLine($"Введенное число {number}, обозначающее день недели является выходным днем");
+else if (number == 7)
+    Console.WriteLine($"Введенное число {number}, обозначающее день недели является выходным днем");
+else
+    Console.WriteLine($"Введенное число {number}, обозначающее день недели не является выходным днем");
