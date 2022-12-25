65805daff864a9f91db74adadfbc9bc6d82678c2
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-25 10:56:03 +0300

    Добавлен проект: решение задачи номер 12

---
+++ Seminar002_Ex_003/Program.cs
@@ -0,0 +1,19 @@
+﻿int ReadNum()
+{
+    int ResNum;
+    while (true)
+    {
+        Console.Write("Введите целое число: ");
+        if (int.TryParse(Console.ReadLine(), out ResNum))
+            break;
+        Console.WriteLine("Ошибка ввода!");
+    }
+    return ResNum;
+}
+int FirstNumber = ReadNum();
+int SecondNumber = ReadNum();
+
+int ost = FirstNumber % SecondNumber;
+
+if (ost == 0) Console.WriteLine($"{FirstNumber}, {SecondNumber} -> кратно");
+else Console.WriteLine($"{FirstNumber}, {SecondNumber} -> не кратно, остаток {ost}");
