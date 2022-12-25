d0b1be0d9ebb193eb2dce2ea1a72de8338e65085
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-25 11:50:46 +0300

    Добавлено решение домашнего задания: Задача 10

---
+++ Seminar002_HW_001/Program.cs
@@ -0,0 +1,13 @@
+﻿int number;
+while (true)
+{
+    Console.Write("Введите целое трехзначное число: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+    if (number > 99 && number <= 999)
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+
+int num = (number % 100) / 10;
+
+Console.WriteLine($"{number} -> {num}");
