66d35c354c4eae8ca96c129772993b4223e1f1e7
IgorMarokhin <lawyer.mii222@gmail.com>
2022-12-25 12:44:17 +0300

    Добавлено решение домашнего задания: Задача 13

---
+++ Seminar002_HW_002/Program.cs
@@ -0,0 +1,27 @@
+﻿int number;
+while (true)
+{
+    Console.Write("Введите целое число: ");
+    if (int.TryParse(Console.ReadLine(), out number))
+        break;
+    Console.WriteLine("Ошибка ввода!");
+}
+
+int num_abs = Math.Abs(number);
+
+if (num_abs < 100)
+{
+    Console.WriteLine($"Третьей цифры нет");
+    return;
+}
+
+int ThirdNumberRight = num_abs / 100 % 10;
+
+while(num_abs > 999)
+{
+    num_abs /= 10;
+}
+int ThirdNumberLeft = num_abs % 10;
+
+Console.WriteLine($"Третья цифра числа {number} справа -> {ThirdNumberRight}");
+Console.WriteLine($"Третья цифра числа {number} слева -> {ThirdNumberLeft}");
